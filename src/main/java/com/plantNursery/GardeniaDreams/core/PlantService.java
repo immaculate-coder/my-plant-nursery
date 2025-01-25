582ec012866cb1229a5b7e142c0bb64533b894fc
Dhawal Patil <dhawalpatil200@gmail.com>
2025-01-25 10:16:59 +0530

    solved comments of controller layer

--- src/main/java/com/plantNursery/GardeniaDreams/core/PlantService.java
+++ src/main/java/com/plantNursery/GardeniaDreams/core/PlantService.java
@@ -1,8 +1,7 @@
 package com.plantNursery.GardeniaDreams.core;
 
-import com.plantNursery.GardeniaDreams.api.request.PlantApiRequest;
-import com.plantNursery.GardeniaDreams.utils.dto.PlantDTO;
+import com.plantNursery.GardeniaDreams.core.model.CreatePlantRequest;
 
 public interface PlantService {
-    void persist(PlantApiRequest plantApiRequest);
+    void persist(CreatePlantRequest createPlantRequest);
 }
