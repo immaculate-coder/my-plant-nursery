b5d78572a4527ee1f21c13b629868eb16aa5c20c
Dhawal Patil <dhawalpatil200@gmail.com>
2025-01-25 10:35:51 +0530

    refactored names and added mappers

--- src/main/java/com/plantNursery/GardeniaDreams/core/PlantPersister.java
+++ src/main/java/com/plantNursery/GardeniaDreams/core/PlantPersister.java
@@ -2,6 +2,6 @@ package com.plantNursery.GardeniaDreams.core;
 
 import com.plantNursery.GardeniaDreams.core.model.CreatePlantRequest;
 
-public interface PlantService {
+public interface PlantPersister {
     void persist(CreatePlantRequest createPlantRequest);
 }
--- src/main/java/com/plantNursery/GardeniaDreams/infra/mongo/MongoPlantStorageManger.java
+++ src/main/java/com/plantNursery/GardeniaDreams/infra/mongo/MongoPlantStorageManger.java
@@ -1,27 +1,28 @@
 package com.plantNursery.GardeniaDreams.infra.mongo;
 
-import com.plantNursery.GardeniaDreams.core.PlantPersistanceService;
+import com.plantNursery.GardeniaDreams.core.PlantStorageManager;
 import com.plantNursery.GardeniaDreams.infra.mongo.entities.PlantDocument;
 import com.plantNursery.GardeniaDreams.infra.mongo.repo.MongoPlantRepository;
 import com.plantNursery.GardeniaDreams.utils.dto.PlantDTO;
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-@Primary
-public class MongoPlantPersistanceService implements PlantPersistanceService {
+public class MongoPlantStorageManger implements PlantStorageManager {
     private MongoPlantRepository repository;
     @Override
-    public void persist(PlantDTO plantDTO) {
-        PlantDocument plantDocument = PlantDocument.builder()
+    public String persist(PlantDTO plantDTO) {
+        return repository.save(from(plantDTO)).getId();
+    }
+
+    // mappers
+    private static PlantDocument from(PlantDTO plantDTO) {
+        return PlantDocument.builder()
                 .name(plantDTO.getName())
                 .wateringIntervalInDays(plantDTO.getWateringIntervalInDays())
                 .ageInDays(plantDTO.getAgeInDays())
                 .lastWateredDate(plantDTO.getLastWateredDate())
                 .build();
-
-        repository.save(plantDocument);
     }
 }
