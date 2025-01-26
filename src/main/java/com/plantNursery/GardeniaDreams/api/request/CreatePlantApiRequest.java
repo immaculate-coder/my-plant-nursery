e9f004dc669f2dc26495bf8c162502999543b022
Dhawal Patil <dhawalpatil200@gmail.com>
2025-01-26 11:41:38 +0530

    plant controller test setup and added test for create new plant

--- src/main/java/com/plantNursery/GardeniaDreams/api/request/CreatePlantApiRequest.java
+++ src/main/java/com/plantNursery/GardeniaDreams/api/request/CreatePlantApiRequest.java
@@ -1,6 +1,9 @@
 package com.plantNursery.GardeniaDreams.api.request;
 
+import lombok.Builder;
+
 import java.util.Date;
 
+@Builder
 public record CreatePlantApiRequest(String name, Integer ageInDays, Date lastWateredDate, Integer wateringIntervalInDays) {
 }
