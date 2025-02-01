1ccfb65b337735e943534ae0021700c61c2ec333
immaculate coder <immaculate.coder@gmail.com>
2025-02-01 20:03:24 +0200

    refactored core service test and make it a unit test instead of integration test

---
+++ src/test/java/com/plantNursery/GardeniaDreams/core/PlantPersistenceServiceTest.java
@@ -0,0 +1,41 @@
+package com.plantNursery.GardeniaDreams.core;
+
+import com.plantNursery.GardeniaDreams.core.model.CreatePlantRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class PlantPersistenceServiceTest {
+    private PlantPersistenceService persistenceService;
+    private PlantStorageManager plantStorageManager;
+
+    @BeforeEach
+    void setUp() {
+        plantStorageManager = mock(PlantStorageManager.class);
+        persistenceService = new PlantPersistenceService(plantStorageManager);
+    }
+
+    @Test
+    void persistPlant_ShouldReturnId() {
+        CreatePlantRequest createPlantRequest = getRequest();
+        String expectedPlantId = "dummy-tree-id-123";
+        when(plantStorageManager.persist(createPlantRequest)).thenReturn(expectedPlantId);
+
+        String actualPlantId = persistenceService.persist(createPlantRequest);
+        verify(plantStorageManager).persist(createPlantRequest);
+        assertEquals(expectedPlantId, actualPlantId, "Plant id mismatch");
+    }
+
+    private static CreatePlantRequest getRequest() {
+        return CreatePlantRequest.builder()
+                .name("Test Plant")
+                .ageInDays(100)
+                .lastWateredDate(new Date())
+                .wateringIntervalInDays(20)
+                .build();
+    }
+}
